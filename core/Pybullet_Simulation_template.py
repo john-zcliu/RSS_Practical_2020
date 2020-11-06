dffd3c85649af260586d80441b1a141f6d74ebf9
john-zcliu <johnlzc0@gmail.com>
2020-11-06 11:53:47 +0000

    Publish V2.1

--- core/Pybullet_Simulation_template.py
+++ core/Pybullet_Simulation_template.py
@@ -177,7 +177,7 @@ class Simulation_template(Simulation_base):
 
     # Task 2.2 Inverse Kinematics
     # Task 2.2.1 Jacobian matrix
-    def jacobianMatrix(self, targetPosition):
+    def jacobianMatrix(self, endEffector, targetPosition):
         """Calculate the Jacobian Matrix for the Nextage Robot"""
         # TODO: Add your code here
         pass
